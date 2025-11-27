0085c32563fe6f4d290be6dad8eed71065743086
seanhill <seamhill93@gmail.com>
2025-11-26 23:48:35 -0500

    large sweep

--- benchmark_stats2.cpp
+++ benchmark_stats2.cpp
@@ -62,8 +62,8 @@ int main(int argc, char** argv) {
 
     // Grid in (n,d)
     // TODO: consider making these command-line parameters as well
-    const int d_values[] = {2, 3, 4};
-    const int n_values[] = {2, 3, 4};
+    const int d_values[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50};
+    const int n_values[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50};
 
     // Open CSV output
     // TODO: when we build this with tasks.json, and run make_plots.py, the latter is looking in /build.
